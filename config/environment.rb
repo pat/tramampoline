a406c8743aea42959ba04a4f758f9c487cf1b1b5
Tim Lucas <t.lucas@toolmantim.com>
2010-04-18 01:43:25 +1000

    Switching back to Rails 2 standard gem requirements.

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'formtastic',    :version => '0.9.7'
+  config.gem 'will_paginate', :version => '2.3.12'
+  config.gem 'exceptional',   :version => '2.0.3'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
