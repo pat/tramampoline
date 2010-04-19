61e8752dd28d25a10b4316311cbd2017c017300e
Pat Allan <pat@freelancing-gods.com>
2010-04-19 23:43:01 +1000

    JS workaround for browsers that don't support HTML5 placeholders.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,4 +12,20 @@
       return false;
     }
   });
+  
+  if(!Modernizr.input.placeholder) {
+    $("input").each(function() {
+      if($(this).val() == "" && $(this).attr("placeholder") != "") {
+        $(this).val($(this).attr("placeholder"));
+        $(this).focus(function() {
+          if ($(this).val() == $(this).attr("placeholder"))
+            $(this).val("");
+        });
+        $(this).blur(function() {
+          if($(this).val() == "")
+            $(this).val($(this).attr("placeholder"));
+        });
+      }
+    });
+  }
 })();
