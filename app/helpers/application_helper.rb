0de0e7ada24769c306b0988ce4a0d314950dbf28
Pat Allan <pat@freelancing-gods.com>
2010-04-19 22:08:28 +1000

    Making views a bit more consistent, and adding copy to warning and sold out pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def referral_link(attendee)
     accept_url(attendee.invite_code)
   end
+  
+  def all_released?
+    Time.now >= Time.local(2010, 4, 29, 10, 0)
+  end
 end
