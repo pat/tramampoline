fd168e028c4a4c47642cc7c566ab4eb5e4366af2
Pat Allan <pat@freelancing-gods.com>
2010-04-29 09:07:17 +1000

    Adding announcement email for extra ticket release in just under an hour.

--- app/models/announcements.rb
+++ app/models/announcements.rb
@@ -5,4 +5,11 @@ class Announcements < ActionMailer::Base
     from        'Pat Allan <pat@freelancing-gods.com>'
     sent_on     Time.zone.now
   end
+  
+  def more(subscriber)
+    subject     'More Tickets for Trampoline!'
+    recipients  subscriber.email
+    from        'Pat Allan <pat@freelancing-gods.com>'
+    sent_on     Time.zone.now
+  end
 end
