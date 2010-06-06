46faf8435459b7c26f44923bfbfba7ec30351dfa
Pat Allan <pat@freelancing-gods.com>
2010-06-06 15:03:51 +1000

    Cleaning up duplicate subscriptions.

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,3 +1,22 @@
 class Subscriber < ActiveRecord::Base
   validates_presence_of :email
+  
+  def self.cleanup!
+    emails = connection.select_values("SELECT DISTINCT email FROM subscribers")
+    emails.each do |email|
+      subscribers = Subscriber.find_all_by_email(email)
+      next if email.length <= 1
+      
+      named   = subscribers.select { |sub| sub.name.present? }
+      unnamed = subscribers - named
+      
+      if named.blank?
+        subscribers[1..-1].each { |sub| sub.destroy }
+      else
+        unnamed.each { |sub| sub.destroy }
+      end
+      
+      named[0..-2].each { |sub| sub.destroy } if named.length > 1
+    end
+  end
 end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,6 +1,37 @@
 require 'spec_helper'
 
 describe Subscriber do
+  describe '.cleanup!' do
+    it "should remove blank named subscribers when one exists with a name and the same email address" do
+      Subscriber.make :name => 'Xavier Shay', :email => 'x@shay.com'
+      Subscriber.make :name => '', :email => 'x@shay.com'
+      
+      Subscriber.cleanup!
+      
+      subscribers = Subscriber.find_all_by_email('x@shay.com')
+      subscribers.length.should == 1
+      subscribers.first.name.should == 'Xavier Shay'
+    end
+    
+    it "should keep one subscriber if there are multiple with no names for the same email address" do
+      Subscriber.make :name => '', :email => 's@sabey.com'
+      Subscriber.make :name => '', :email => 's@sabey.com'
+      
+      Subscriber.cleanup!
+      
+      Subscriber.find_all_by_email('s@sabey.com').length.should == 1
+    end
+    
+    it "should remove all but one named subscriber for each email address" do
+      Subscriber.make :name => 'Peter Spence', :email => 'p@spence.com'
+      Subscriber.make :name => 'Peter Spence', :email => 'p@spence.com'
+      
+      Subscriber.cleanup!
+      
+      Subscriber.find_all_by_email('p@spence.com').length.should == 1
+    end
+  end
+  
   describe '#valid?' do
     it "should be invalid without an email address" do
       subscriber = Subscriber.make_unsaved :email => nil
