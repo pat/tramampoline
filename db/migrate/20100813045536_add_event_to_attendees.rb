52a50a3b061560ab7488fa81db8675f02a205994
Pat Allan <pat@freelancing-gods.com>
2010-08-15 17:10:18 +1000

    A bit of a hack, but didn't realise I was running MySQL locally, and PostgreSQL on production is smart enough to see through my ruse of adding a new column that doesn't allow NULLs, but having no default.

--- db/migrate/20100813045536_add_event_to_attendees.rb
+++ db/migrate/20100813045536_add_event_to_attendees.rb
@@ -1,6 +1,6 @@
 class AddEventToAttendees < ActiveRecord::Migration
   def self.up
-    add_column :attendees, :event_id, :integer, :null => false
+    add_column :attendees, :event_id, :integer, :null => false, :default => Event.past.last.id
     add_index :attendees, :event_id
   end
 
