3983bdaaa4ac1ec0280998833167d8d6c24b9f20
Pat Allan <pat@freelancing-gods.com>
2011-02-13 22:44:47 +1100

    Outputting hidden emissions details.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  include TronprintHelper
+  
   def next_event
     @next_event ||= Event.next
   end
