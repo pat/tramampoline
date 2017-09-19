af0e16cd1332c14b4c392c8c33d929227eda0eef
Pat Allan <pat@freelancing-gods.com>
2017-09-19 22:28:50 +0200

    Direct reference to middleware rather than via a String.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,5 +51,5 @@ Tramampoline::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.middleware.use 'SingleDomain'
+  config.middleware.use SingleDomain
 end
